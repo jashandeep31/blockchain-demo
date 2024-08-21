7786ccc2880ff5a12bd2b1942acea7b1276c10a8
jashandeep <jashandeep1659@gmail.com>
2024-08-21 22:24:14 +0530

    feat: .env support

--- miner/src/index.ts
+++ miner/src/index.ts
@@ -6,12 +6,14 @@ import { createAccountFunction } from "./lib/utils.js";
 import { initializeSocket } from "./socket_setup.js";
 import routes from "./routes/index.js";
 import { io } from "socket.io-client";
-
+import dotenv from "dotenv";
 // details of the miner account where after moeny will deposited
 export const MY_ACCOUNT = createAccountFunction();
 export const blockchain = new Blockchain();
 const socketConnection = io(process.env.SERVER_URL ?? "");
 export let MY_ID = "";
+
+dotenv.config();
 
 // basic socket connection to get out MY_ID
 socketConnection.on("connect", () => {
